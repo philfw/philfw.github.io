1448b8f16994ace15d87875f7d58d4cd6e443dd5
Phil Weiss <philweiss@phils-air.mynetworksettings.com>
2024-10-08 21:13:35 -0400

    still debugging

--- index.mjs
+++ index.mjs
@@ -57,9 +57,7 @@ window.onload = (event) => {
 
   submitButton.addEventListener('click', (event) => {
     var userInput = document.getElementById('ai-input').value;
-    if (userInput == '') {
-      console.log('no text');
-    } else { sendReq(userInput); }
+    sendReq(userInput);
   });
 
 };
